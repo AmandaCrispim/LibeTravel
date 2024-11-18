930a7b652de474fdd1ed6b6c86a0cca48837d5ba
AmandaCrispim <amandacrispim384@gmail.com>
2024-11-18 11:04:45 -0300

    update: repositories

--- src/main/java/br/edu/univille/poo/libetravel/repositories/PassagemRepository.java
+++ src/main/java/br/edu/univille/poo/libetravel/repositories/PassagemRepository.java
@@ -2,6 +2,8 @@ package br.edu.univille.poo.libetravel.repositories;
 
 import br.edu.univille.poo.libetravel.entities.Passagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PassagemRepository extends JpaRepository<Passagem, Long> {
 }
